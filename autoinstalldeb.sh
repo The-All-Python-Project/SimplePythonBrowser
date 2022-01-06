65c51b8bba345561d09fcc072a7b3283b98768ee
JohnVictoryz <85512286+JohnVictoryz@users.noreply.github.com>
2022-01-06 16:25:30 +0200

    Add files via upload

---
+++ autoinstalldeb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt install cat
+git 
+sudo apt install git
+git clone https://www.github.com/JohnVictoryz/SimplePythonBrowser
+cd SimplePythonBrowser
+cat LICENSE
+python --version
+python3 --version
+sudo apt-get update
+sudo apt-get install python
+sudo apt update
+sudo apt install python-pip
+pip --version
+sudo apt update
+sudo apt install python3-pip
+pip3 --version
+pip install PyQt5
+pip install PyQtWebEngine
+pip install sip
+pip install requests
