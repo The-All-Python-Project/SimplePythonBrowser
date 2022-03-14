640152b4c4b08eb85de15cd545e3bf457615d3a4
JohnVictoryz <john.g.zioupos@gmail.com>
2022-03-14 22:00:58 +0200

    Update autorundeb.sh

--- autorundeb.sh
+++ autorundeb.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# If you want it to cd in the directory for easy acess to the program uncomment the next line below
+# cd YOURFILEPATHTOTHEDIRECTORIYOUINSTALL
 python Simple_browser.py
 python3 Simple_browser.py
