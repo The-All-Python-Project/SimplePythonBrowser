3a253ebfb9d7f21b0b088889653b08952c130e2d
JohnVictoryz <john.g.zioupos@gmail.com>
2022-03-16 15:54:26 +0200

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # If you want it to cd in the directory for easy acess to the program uncomment the next line below
-# cd YOURFILEPATHTOTHEDIRECTORIYOUINSTALL
+# cd YOURFILEPATHTOTHEDIRECTORIYOUINSTALLED
 python Simple_browser.py
 python3 Simple_browser.py
